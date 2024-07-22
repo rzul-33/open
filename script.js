3b1eede0707c1f031ff48e7dedb360a1d9f0027a
rzul-33 <rijulsuwal333@gmail.com>
2024-07-22 16:35:58 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+ yesBtn.addEventListener ('click', ()=> {
+  question.innerHTML = 'I knew it you would say yes, Tommorow at 2';
+ });
+  
+ noBtn.addEventListener('mouseover', ()=> {
+    const i = Math.floor(Math.random() *(wrapperRect.width - noBtnRect.width)) + 1.5;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) +1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px' ;
+ })
